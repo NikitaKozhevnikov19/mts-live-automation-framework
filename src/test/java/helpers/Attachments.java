4a550a698f5b5ff280d979bb6144760e544d7c2a
Nikita Kozhevnikov <kozherka2@gmail.com>
2026-02-10 17:37:06 +0500

    changed Attachments.java

--- src/test/java/helpers/Attachments.java
+++ src/test/java/helpers/Attachments.java
@@ -1,12 +1,15 @@
 package helpers;
 
+import com.codeborne.selenide.Selenide;
 import io.qameta.allure.Attachment;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 
 import java.nio.charset.StandardCharsets;
 
+import static com.codeborne.selenide.Selenide.sessionId;
 import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
+import static org.openqa.selenium.logging.LogType.BROWSER;
 
 public class Attachments {
 
@@ -20,10 +23,24 @@ public class Attachments {
         return getWebDriver().getPageSource().getBytes(StandardCharsets.UTF_8);
     }
 
+    // Для веба (Selenoid)
+    @Attachment(value = "Video", type = "text/html", fileExtension = ".html")
+    public static String addVideo() {
+        return "<html><body><video width='100%' height='100%' controls autoplay><source src='"
+                + "https://selenoid.autotests.cloud" + sessionId() + ".mp4"
+                + "' type='video/mp4'></video></body></html>";
+    }
+
+    // Для мобилок (Browserstack)
     @Attachment(value = "Video", type = "text/html", fileExtension = ".html")
     public static String addVideo(String sessionId) {
         return "<html><body><video width='100%' height='100%' controls autoplay><source src='"
                 + helpers.Browserstack.videoUrl(sessionId)
                 + "' type='video/mp4'></video></body></html>";
+    }
+
+    @Attachment(value = "Browser console logs", type = "text/plain")
+    public static String browserConsoleLogs() {
+        return String.join("\n", Selenide.getWebDriverLogs(BROWSER));
     }
 }
